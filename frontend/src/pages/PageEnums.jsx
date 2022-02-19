6d19d0786731640ec9ab72ef772f0ceb9a8b8660
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-19 16:41:22 -0700

    feat: Added slice/reducer for routing

---
+++ frontend/src/pages/PageEnums.jsx
@@ -0,0 +1,7 @@
+
+// Enums for use in navigation routing
+//    'enums' as a type does not exist in JS so consts are used instead
+export const History = "history";
+export const Dashboard = "dashboard";
+export const Peers = "peers";
+export const Settings = "settings";
