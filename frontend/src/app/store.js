d494f82364ec2ced4fcf99bc54854b5afa182f8d
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-19 16:18:39 -0700

    feat: Added user to App's redux store in store.jsx

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../app/reducers/UserSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    user: userReducer,
   },
 });
