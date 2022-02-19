5da1d112fe35a58c50de650eccab91ec8e25ea72
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-02-19 13:54:37 -0700

    refactor: Moved core API
    - Added app directory to contain core API code

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,4 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+import uvicorn
 
-app = FastAPI()
-
-templates = Jinja2Templates(directory="../frontend/build/")
-app.mount("/build", StaticFiles(directory="../frontend/build/"), name="build")
-
-
-@app.get("/")
-def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+if __name__ == "__main__":
+    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
