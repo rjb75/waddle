fd2bc389f3a14fccc3e26a64abb99e9539269850
Robert Brown <brown.rjb75@gmail.com>
2022-02-20 05:12:09 -0700

    feat: add initial question components

---
+++ frontend/src/components/Questions/Question.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import {QuestionTypes} from './QuestionConstants';
+import TextField from '../Inputs/TextField';
+import Picker from 'emoji-picker-react';
+import './Questions.scss';
+
+const Question = (props) => {
+    const {type, question} = props;
+    const [answer, setAnswer] = useState([]);
+
+    const handleEmojiSelect = (e, emoji) => {
+        console.log(emoji)
+        setAnswer(answer => [...answer, emoji]);
+    };
+
+    return  (
+        <div className="question-component text--black text-a-c">
+            <p className="question-text">{question}</p>
+            {
+                type == QuestionTypes.keyword ?
+                <TextField type="text" placeholder="Enter Your Response" /> :
+                <Picker onEmojiClick={handleEmojiSelect}/>
+            }
+        </div>
+    );
+};
+
+export default Question;
