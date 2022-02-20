87a9a4aa9c987c369222bb2c40fe9e0ba034c10c
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-02-20 05:09:42 -0700

    feat: add routers

--- backend/app/api.py
+++ backend/app/api.py
@@ -3,11 +3,13 @@ from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
 
 from app.dependencies import auth
-from app.routers import questions, users
+from app.routers import questions, responses, supports, users
 
 app = FastAPI()
-app.include_router(users.router)
+app.include_router(responses.router)
+app.include_router(supports.router)
 app.include_router(questions.router)
+app.include_router(users.router)
 
 
 @app.post("/token")
