9cfd810d941869b74063ae285e3ffc142225dcb7
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-20 03:52:25 -0700

    feat: Can now detect when login credentials are incorrect

--- frontend/src/components/Authentication/AuthenticationFunctions.jsx
+++ frontend/src/components/Authentication/AuthenticationFunctions.jsx
@@ -7,12 +7,34 @@ export function passwordsMatch(props) {
 
 export function submitLogin(props) {
   let form = new FormData();
+  var respStatus;
+  var returnStat;
   form.append("username", props.username);
   form.append("password", props.password);
   axiosFORMInst
     .post("/token", form)
-    .then(console.log("done"))
-    .catch((err) => console.log(err));
+    .then(function (res) {
+      console.log(res.data);
+      console.log(res.status);
+      console.log(res.statusText);
+      console.log(res.headers);
+      console.log(res.config);
+      respStatus = res.status;
+      return true;
+    })
+    .catch((err) => {
+      if (err.response) {
+        console.log(err.response.status);
+        returnStat = false;
+      }
+      console.log(JSON.stringify(err.toJSON()));
+      console.log(err.status);
+      console.log(err.statusText);
+      console.log(err.headers);
+      console.log(err.config);
+      respStatus = err.statusTest;
+    });
+  return returnStat;
 }
 
 export function submitRegistration(props) {
