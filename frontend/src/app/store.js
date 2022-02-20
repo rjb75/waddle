4cc042a00cd5a83f946fd4c677e266ce56a8e1c8
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-19 23:21:34 -0700

    feat: Added authentication store to tract logged in or non-logged in state

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import userReducer from '../app/reducers/UserSlice';
 import routingReducer from '../app/reducers/RoutingSlice';
+import authReducer from '../app/reducers/AuthenticationSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     user: userReducer,
     routing: routingReducer,
+    auth: authReducer,
   },
 });
