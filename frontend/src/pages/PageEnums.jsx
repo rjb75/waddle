37f948e44a37ff35e93a926dee591962c0d07246
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-19 23:21:09 -0700

    refactor: Updated page enums to be an object instead of individual consts

--- frontend/src/pages/PageEnums.jsx
+++ frontend/src/pages/PageEnums.jsx
@@ -1,6 +1,13 @@
-
 // Enums for use in navigation routing
 //    'enums' as a type does not exist in JS so consts are used instead
+export const Pages = {
+  History: "history",
+  Dashboard: "dashboard",
+  Peers: "peers",
+  Settings: "settings",
+  Login: "login",
+  Registration: "registration",
+};
 export const History = "historyBEEF";
 export const Dashboard = "dashboard";
 export const Peers = "peers";
--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import NavBar from "../components/NavBar";
 
 const Settings = () => {
-    return (
-        <div className="page-content settings">
-            <h2>Settings Page</h2>
-        </div>
-    )
-}
+  return (
+    <div className="page-content settings">
+      <h2>Settings Page</h2> <NavBar />
+    </div>
+  );
+};
 
 export default Settings;
