646a5c97ee41a32a42f3ac83f25b5f9cef513b9b
Robert Brown <brown.rjb75@gmail.com>
2022-02-20 10:16:23 -0700

    feat: add questions to screen

--- frontend/src/components/Questions/Questions.jsx
+++ frontend/src/components/Questions/Questions.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axiosJSONInst from "../../AxiosJSON";
+import Question from "./Question";
 
-const Questions = () => {
+const Questions = (props) => {
+    const {onClose} = props;
     const [questions, setQuestions] = useState([]);
     const [selectedQuestions, setSelectedQuestions] = useState([]);
 
@@ -9,18 +11,34 @@ const Questions = () => {
         axiosJSONInst
             .get("/questions")
             .then((res) => {
-                setQuestions(res)
+                setQuestions(res.data)
             })
             .catch((err) => console.error(err))
     }, [])
 
     useEffect(() => {
-        console.log(questions)
-    }, [questions, selectedQuestions])
+        if(questions.length > 0) {
+            const rand1 = Math.floor(Math.random() * (questions.length - 1))
+            const rand2 = Math.floor(Math.random() * (questions.length - 1))
+            const rand3 = Math.floor(Math.random() * (questions.length - 1))
+            setSelectedQuestions([questions[rand1], questions[rand2], questions[rand3]])
+        }
+    }, [questions])
+
+    const handleSubmit = () => {
+        onClose()
+    }
 
     return (
         <div>
-
+            {
+                selectedQuestions && <div>
+                    { selectedQuestions[0] && <Question {...selectedQuestions[0]} />}
+                    { selectedQuestions[1] && <Question {...selectedQuestions[1]} />}
+                    { selectedQuestions[2] && <Question {...selectedQuestions[2]} />}
+                </div>
+            }
+            <p className="btn btn-primary--blue-vibrant" onClick={handleSubmit} >Submit Questions</p>
         </div>
     );
 };
