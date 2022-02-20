e6db9c3516667a7205b44c4878ecc71c5dc3bb7e
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-19 23:08:13 -0700

    feat: added ability to submit user login info from login

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -1,31 +1,47 @@
 import React from "react";
 import { useState } from "react";
 import TextField from "../Inputs/TextField";
-import './Authentication.scss';
+import "./Authentication.scss";
+import { submitLogin } from "./AuthenticationFunctions";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState('');
+  const handleLogin = () => {
+    console.log({ email: email, password: password });
+    submitLogin({ email: email, password: password });
+  };
 
-    const handleLogin = () => {
-        // TODO: handle login
-    }
-
-    return (
-        <div className="login-container auth-child flex-c">
-            <div className="header-container flex-c">
-                <h2 className="title">Login</h2>
-                {errors && <p className="error-text text--red-dark">{errors}</p>}
-            </div>
-            <div className="field-container flex-c">
-                <TextField type={'email'} placeholder={'Enter your Email'} fieldValue={email} setValue={(e) => setEmail(e)} />
-                <TextField type={'password'} placeholder={'Enter your Password'} fieldValue={password} setValue={(e) => setPassword(e)} />
-                <p className="btn btn-primary--blue-vibrant action-button all-caps" onClick={handleLogin}>Sign In</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="login-container auth-child flex-c">
+      <div className="header-container flex-c">
+        <h2 className="title">Login</h2>
+        {errors && <p className="error-text text--red-dark">{errors}</p>}
+      </div>
+      <div className="field-container flex-c">
+        <TextField
+          type={"email"}
+          placeholder={"Enter your Email"}
+          fieldValue={email}
+          setValue={(e) => setEmail(e)}
+        />
+        <TextField
+          type={"password"}
+          placeholder={"Enter your Password"}
+          fieldValue={password}
+          setValue={(e) => setPassword(e)}
+        />
+        <button
+          className="btn btn-primary--blue-vibrant action-button all-caps"
+          onClick={() => handleLogin()}
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
