9e556f37461fc9dc10a1eb7aca9408150dca4d6b
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-02-20 02:02:03 -0700

    refactor: move routes to users

--- backend/app/api.py
+++ backend/app/api.py
@@ -2,9 +2,6 @@ from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
 
-import httpx
-import asyncio
-
 from app.dependencies import auth
 from app.routers import questions, sentiment, users
 
@@ -13,25 +10,6 @@ app.include_router(users.router)
 app.include_router(questions.router)
 app.include_router(sentiment.router)
 
-#Change Later
-url = "http://localhost:3000"
-
-
-#DB Microservice Redirection
-@app.post('/db/user')
-async def f():
-     async with httpx.AsyncClient() as client:
-        response = await client.post(url + "/api/v1/user", data={
-            'Email': 'horses@horses', 
-            'Name': 'Da Horse',
-            'Pin': "51231"})
-        return response.text
-
-@app.get('/db/user/:user')
-async def f():
-     async with httpx.AsyncClient() as client:
-        response = await client.get(url + "/api/v1/user/" + '12f1df04-9215-11ec-8022-122907f6e3b9')
-        return response.text
 
 @app.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
