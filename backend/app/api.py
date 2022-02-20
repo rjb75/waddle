cd34e269b95cb0656eaa48dbee7a6b34fe66cb5c
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-02-19 17:05:40 -0700

    feat: Dummy Auth
    - OAuth2 setup with fake user info and hashing

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
-from fastapi.templating import Jinja2Templates
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
-from app.routers import users, questions
+
+from app.dependencies import auth
+from app.routers import questions, users
 
 app = FastAPI()
-
 app.include_router(users.router)
 app.include_router(questions.router)
 
-templates = Jinja2Templates(directory="../frontend/build/")
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = auth.fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = auth.UserInDB(**user_dict)
+    hashed_password = auth.fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
 app.mount("/", StaticFiles(directory="../frontend/build/", html=True), name="build")
---
+++ backend/app/dependencies/auth.py
@@ -0,0 +1,56 @@
+from typing import Optional
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+    },
+    "alice": {
+        "username": "alice",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+    },
+}
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
