91422f0c69d541ce4a76732e19dde91058d91c55
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-02-20 02:03:43 -0700

    feat: add password to user model

--- backend-go/src/models/user.go
+++ backend-go/src/models/user.go
@@ -1,8 +1,9 @@
 package models
 
 type User struct {
-	User_id string `json:"User_id"`
-	Email string `json:"Email"`
-	Pin int `json:"Pin"`
-	Name string `json:"Name"`
+	User_id  string `json:"User_id"`
+	Email    string `json:"Email"`
+	Pin      int    `json:"Pin"`
+	Name     string `json:"Name"`
+	Password string `json:"Password"`
 }
