690c1ca7ed7e3577d4d0e8c0e94ec40d3e5206b5
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-19 20:13:28 -0700

    feat: Added skeleton for login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <label>
+          Enter your Email
+          <input type="type" name="email" />
+        </label>
+        <label>
+          Enter your Password
+          <input type="type" name="password" />
+        </label>
+        <button>Sign In</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
