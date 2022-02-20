c6e2b13683b62395529145a4ea82e891b8abfce0
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-02-19 17:05:40 -0700

    feat: Dummy Auth
    - OAuth2 setup with fake user info and hashing

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,3 +1,9 @@
-from fastapi import APIRouter
+from app.dependencies import auth
+from fastapi import APIRouter, Depends
 
 router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.get("/me")
+async def read_current_user(current_user: auth.User = Depends(auth.get_current_user)):
+    return current_user
