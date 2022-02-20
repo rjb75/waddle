e26d45724767c7823261f1075be10010f031325d
Risat Haque <risathq@gmail.com>
2022-02-19 22:21:45 -0700

    fix: missing go file

---
+++ backend-go/src/routes.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	//	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterRoutes(app *fiber.App) {
+
+	//Api Version Configuration
+	api := app.Group("/api") // /api
+	v1 := api.Group("/v1")   // /api/v1
+
+	personRoutes(v1)
+
+}
+
+
+
+func personRoutes(v fiber.Router) {
+	v.Get("/test", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	  })
+
+
+}
+
+
